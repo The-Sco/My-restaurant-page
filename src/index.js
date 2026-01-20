317a3cd23e3c0e6eb529f52c9ef4993eb8f160fc
The-Sco <goodmorningvo3@gmail.com>
2026-01-20 22:51:41 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import style from "./styles/global.css"
-import { addNavigation } from "./functions";
 import createNavBar from "./header-footer";
 import loadHomePage from "./home";
-import loadMenuPage from "./menu"
-import loadAboutPage from "./about";
+
 
 window.location.hash = "home";
 
 createNavBar();
-loadAboutPage();
+loadHomePage();
